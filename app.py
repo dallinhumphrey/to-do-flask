8e9cc17d8c31b7ceaa31957d117353e7965234bf
dallinhumphrey <dallinhumphrey@gmail.com>
2020-04-08 10:05:15 -0600

    removed irrelevent code

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORSfrom flask_heroku import Heroku
 import os
 
 app = Flask(__name__)
+CORS(app)
+heroku = Heroku(app)
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
 
@@ -45,7 +49,6 @@ def get_todo(id):
 # POST
 @app.route("/todo", methods=["POST"])
 def add_todo():
-  data 
   title = request.json["title"]
   done = request.json["done"]
   new_todo = Todo(title, done)
